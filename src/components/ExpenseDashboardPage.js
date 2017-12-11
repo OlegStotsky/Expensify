4d9579d0aa71252e1b2046ac44214106efc9295c
Oleg Stotsky <ostotsky@gmail.com>
2017-12-11 10:15:16 +0300

    add expense filtering

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ExpenseList from './ExpenseList.js';
+import ExpenseListFilters from './ExpenseListFilters.js';
 
 const ExpenseDashboardPage = () => (
   <div>
+      <ExpenseListFilters />
       This is from my dashboard component
       <ExpenseList />
   </div>
