20f7fbb23eda1b046b392c058437458a03e51131
Oleg Stotsky <ostotsky@gmail.com>
2017-12-12 08:17:35 +0300

    add expense filtering by date

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { removeExpense } from '../actions/expenses.js';
+import moment from 'moment';
 
 const ExpenseListItem = (props) => (
   <div>
@@ -9,7 +10,7 @@ const ExpenseListItem = (props) => (
       <Link to={`/edit/${props.expense.id}`}>Edit</Link>
       <li>Description: {props.expense.description}</li>
       <li>Amount: {props.expense.amount}</li>
-      <li>Created at: {props.expense.createdAt}</li>
+      <li>Created at: {moment(props.expense.createdAt).format('D/MM/YYYY')}</li>
       <button onClick={() => {
         console.log('hello');
         props.dispatch(removeExpense(props.expense.id));
