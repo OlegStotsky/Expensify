513fa6113875e22fb3b2a29be0a37421ed95555f
Oleg Stotsky <ostotsky@gmail.com>
2017-12-13 11:41:56 +0300

    add heroku setup

--- server/server.js
+++ server/server.js
@@ -3,10 +3,12 @@ const path = require('path');
 
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+
 app.use(express.static(publicPath));
 
 app.get('*', (request, response) => {
   response.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(3000);
+app.listen(port);
